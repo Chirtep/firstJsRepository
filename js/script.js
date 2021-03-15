ac811439aadf10792aeba900c2fca309d92572bd
Chirtep <sergey2716@gmail.com>
2021-03-16 03:31:03 +0400

    fix

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 "use strict";
 
-// for (let i = 1; i < 3; i++) {
-
-//     let a = prompt('Один из последних просмотренных фильмов?', '');
-//     let b = prompt('На сколько оцените его?','');
-// }
-
 const numberOfFilms = +prompt('Сколько фильмов уже посмотрел?');
 
 if (numberOfFilms < 10) {
@@ -20,19 +14,36 @@ if (numberOfFilms < 10) {
     alert('ТЫ ВСЕ СЛОМАЛ!');
 }
 
+// for (let i = 0; i < 2; i++) {
+
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?','');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+
+
 let a, b;
 
 function ask (n, string) {
     n = prompt (string);
-    if (n == false || n.length > 50) {
-        return ask (n, string);
-    }
+    
 }
 
-for (let i = 1; i < 3; i++) {
+for (let i = 0; i < 2; i++) {
 
         ask (a, 'Какой фильм посмотрел недавно?');
         ask (b, 'На сколько оценишь?');
+
+        if (a == '' && b == '' && a.length > 50 && a == null && b == null) {
+            console.log('error');
+            i--;
+        }
     }
 
 
