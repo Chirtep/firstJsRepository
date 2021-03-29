e8d4babfc29bf3c778ccc81e408ed7b937922a83
Chirtep <sergey2716@gmail.com>
2021-03-29 23:18:36 +0400

    DOM

--- js/script.js
+++ js/script.js
@@ -1,54 +1,18 @@
-"use strict";
+// console.log(document.head);
+// console.log(document.documentElement);
+console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastChild);
 
-// To string
+// console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentElement);
 
-// 1)
-console.log(typeof(String(null)));
-console.log(typeof(String(4)));
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
 
-// 2)
-console.log(typeof(null + ''));
-
-const num = 5;
-
-console.log('https://vk.com/catalog/' + num);
-
-const fontSize = 26 + 'px';
-
-// To Number
-
-// 1)
-console.log(typeof(Number('4')));
-
-// 2)
-console.log(typeof(+ '5'));
-
-// 3)
-console.log(typeof(parseInt('15px', 10)));
-
-
-let answer = +prompt("Hello", "");
-
-// To boolean
-
-// 0, '', null, undefined, NaN;
-
-
-// 1)
-let switcher = null;
-
-if (switcher) {
-    console.log('Working...');
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+    console.log(node);
 }
-
-switcher = 1;
-
-if (switcher) {
-    console.log('Working...');
-}
-
-// 2)
-console.log(typeof(Boolean('4')));
-
-// 3)
-console.log(typeof(!!'4444'));
