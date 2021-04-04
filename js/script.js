43cc1f09831e3bea0ba8dce782742d8b4713f9e0
Chirtep <sergey2716@gmail.com>
2021-04-05 01:40:27 +0400

    -a'

--- js/script.js
+++ js/script.js
@@ -1,18 +1,39 @@
-// console.log(document.head);
-// console.log(document.documentElement);
-console.log(document.body.childNodes);
-// console.log(document.body.firstChild);
-// console.log(document.body.firstElementChild);
-// console.log(document.body.lastChild);
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
 
-// console.log(document.querySelector('#current').parentNode);
-// console.log(document.querySelector('#current').parentElement);
+// console.log(btns[0].classList.item(1));
+// console.log(btns[1].classList.add('red', 'aboba'));
+// console.log(btns[0].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue'));
 
-// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// }
 
-for (let node of document.body.childNodes) {
-    if (node.nodeName == '#text') {
-        continue;
+btns[0].addEventListener('click', () => {
+    // if (!btns[1].classList.contains('red')) {
+    //     btns[1].classList.add('red');
+    // } else {
+    //     btns[1].classList.remove('red');
+    // }
+
+    btns[1].classList.toggle('red');
+});
+
+// console.log(btns[0].className);
+
+wrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.matches("button.red")) {
+        console.log('Hello');
     }
-    console.log(node);
-}
+});
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('Hello');
+//     });
+// });
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
