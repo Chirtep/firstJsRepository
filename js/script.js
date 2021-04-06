1d14bc531b8914b68a65990a561d85c3f4ceb626
Chirtep <sergey2716@gmail.com>
2021-04-07 02:04:13 +0400

    Работа с датами

--- js/script.js
+++ js/script.js
@@ -1,35 +1,25 @@
-const btn = document.querySelector('.btn');
-let timerId,
-    i = 0;
+'use strict';
 
-function myAnimation() {
-    const elem = document.querySelector('.box');
-    let pos = 0;
+const now = new Date('2021-04-07');
+// new Date.parse('2021-04-07');
 
-    const id = setInterval(frame, 10);
-    function frame() {
-        if(pos == 300) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
-        }
-    }
+console.log(now.setHours(40));
+console.log(now);
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+let start = new Date();
+
+for(let i = 0; i < 100000; i++) {
+    let some = i ** 3;
 }
 
+let end = new Date();
 
-btn.addEventListener('click', myAnimation);
-
-// function logger () {
-//     if (i === 3) {
-//         clearInterval(timerId);
-//     }
-//     console.log('text');
-//     i++;
-// }
-
-// let id = setTimeout(function log(){
-//     console.log('Hello');
-//     id = setTimeout(log, 500);
-// }, 500);
+alert(`Цикл отработал за ${end - start} миллисекунд`);
