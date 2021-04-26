f929ff176d4781114974b3b99dbfb57acf0a39f9
Chirtep <sergey2716@gmail.com>
2021-04-26 23:35:43 +0400

    Модули

--- js/script.js
+++ js/script.js
@@ -1,63 +1,25 @@
 'use strict';
 
-// class User {
-// constructor(name, age) {
-//     this.name = name;
-//     this._age = age; 
-// }
+const app = '123';
 
-// #surname = 'Petrichenko';
-    
+const number = 1;
 
-//     say = () => {
-//         console.log(`Имя пользователя: ${this.name} ${this.#surname}, возраст: ${this._age} `);
-//     }
+(function (){
+    let number = 2;
+    console.log(number);
+    console.log(number + 3);
+}());
 
-//     get age() {
-//         return this._age;
-//     }
+console.log(number);
 
-//    set age(age) {
-//         if (typeof age === 'number' && age > 0 && age < 110) {
-//             this._age = age;
-//         } else {
-//             console.log('Недопустимое значение');
-//         }
-//     }
-// }
+const user = (function(){
+    const privat = function() {
+        console.log('I am privat');
+    };
 
-// const ivan = new User('Ivan', 27);
-// console.log(ivan.surname);
-// ivan.say();
+    return {
+        sayHello: privat
+    };
+}());
 
-
-class Worker {
-constructor(name, surname, age) {
-    this._name = name;
-    this._surname = surname;
-    this._age = age; 
-}
-
-#specialty = 'Operator';
-    
-
-    say = () => {
-        console.log(`Имя пользователя: ${this._name} ${this._surname}, возраст: ${this._age}, профессия: ${this.#specialty}`);
-    }
-
-//     get specialty() {
-//         return this.#specialty;
-//     }
-
-//    set specialty(_specialty) {
-//         if (_specialty == 'Operator') {
-//             this._specialty = 'JS Developer'
-//         } else {
-//             console.log('Работай');
-//         }
-//     }
-}
-
-const sergei = new Worker('Sergei', 'Petrichenko', 27);
-
-sergei.say();
+user.sayHello();
